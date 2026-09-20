fafd188235e3af154fc2819251b40a4c97c08079
kamol666 <ktoshpulatov17@gmail.com>
2026-09-21 00:32:19 +0500

    Revert "Fix landing desktop hero and logo fallback"
    This reverts commit 224a1f37e984ae99cd9feeddece68e73c44795ab.

--- components/logo.tsx
+++ components/logo.tsx
@@ -62,13 +62,7 @@ export function LogoMark({
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
     >
-      <svg
-        viewBox="0 0 512 512"
-        width="1em"
-        height="1em"
-        role="img"
-        focusable="false"
-      >
+      <svg viewBox="0 0 512 512" role="img" focusable="false">
         <defs>
           <linearGradient
             id="chatspace-logo-gradient"
